3d128d904fe72954bc2afc2e75272c5ce126f8fd
adriaaCosta <adria.c@outlook.com>
2020-08-17 23:09:45 -0400

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+//app.use(routes);
+
+app.post('/users', (request, response)=>{
+    const { name, email } = request.body;
+    const user = {
+        name,
+        email,
+    };
+
+    return response.json(user);
+});
+
+app.listen(3333, () =>{
+    console.log('🤖 Server started on port 3333!');
+});
